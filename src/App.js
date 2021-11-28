1a240fd06cfd39c1131b188fe6234cb7624fcbb6
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2021-11-28 12:31:55 +0200

    Statistics initial markup added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Profile from "./components/Profile/Profile";
 import user from "./components/Profile/user.json";
+import Statistics from "./components/Statistics/Statistics";
+import statData from "./components/Statistics/data.json";
 
 function App() {
   return (
     <div>
       <Profile {...user} />
+      <Statistics data={statData} />
     </div>
   );
 }
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,1 +1,20 @@
+import PropTypes from "prop-types";
 import s from "./Statistics.module.css";
+
+function Statistics({ data, title }) {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+      <ul className={s.statList}>
+        {data.map((item) => (
+          <li key={item.id} className={s.item}>
+            <span className={s.label}>{item.label}</span>
+            <span className={s.percentage}>{item.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Statistics;
